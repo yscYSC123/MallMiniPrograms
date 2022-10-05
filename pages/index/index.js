7b4e194711fc41ebae3a65ba1789cd6d16532f3f
yscYSC123 <719122203@qq.com>
2022-10-05 14:43:26 +0800

    完成推荐商品显示功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,10 +3,12 @@ import {config} from "../../request/config.js";
 Page({
     data:{
         defaultImageUrl:'../../imgs/default.png',
-        goodsInfoCarouselList:[]    //轮播图列表
+        goodsInfoCarouselList:[],    //轮播图列表
+        goodsInfoRecommendList:[],     //推荐商品列表
     },
     onLoad: function(){
         this.getGoodsInfoCarouselList();
+        this.goodsInfoRecommendList();
     },
 
     //获取轮播图
@@ -33,6 +35,40 @@ Page({
                 }
                 this.setData({
                     goodsInfoCarouselList
+                })
+            }else{
+                wx.showToast({
+                  title: res.msg,
+                  icon:'none'
+                })
+            }
+        })
+    },
+
+    //获取推荐商品
+    goodsInfoRecommendList(){
+        request({url: '/goodsInfo/findRecommendGoods'}).then(res => {
+            if(res.code === '0'){
+                let goodsInfoRecommendList = res.data.list;
+                if(!goodsInfoRecommendList || goodsInfoRecommendList.length === 0){
+                    goodsInfoRecommendList.push({"name":"名称1","url":this.data.defaultImageUrl});
+                    goodsInfoRecommendList.push({"name":"名称2","url":this.data.defaultImageUrl});
+                    goodsInfoRecommendList.push({"name":"名称3","url":this.data.defaultImageUrl});
+                }else{
+                    if(goodsInfoRecommendList.length>4){
+                        goodsInfoRecommendList = goodsInfoRecommendList.slice(0,4);
+                    }
+                    goodsInfoRecommendList.forEach(item => {
+                        if(!item.fields||item.fields==='[]'){
+                            item.url = this.data.defaultImageUrl;
+                        }else{
+                            let fileArr = JSON.parse(item.fields);
+                            item.url = config.baseFileUrl+fileArr[0];
+                        }
+                    });
+                }
+                this.setData({
+                    goodsInfoRecommendList
                 })
             }else{
                 wx.showToast({
