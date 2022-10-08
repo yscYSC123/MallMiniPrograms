d189451b1a2a64b19656aa587271f217197365fa
yscYSC123 <719122203@qq.com>
2022-10-08 20:07:45 +0800

    实现小程序本站商品列表显示

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,11 +6,13 @@ Page({
         goodsInfoCarouselList:[],    //轮播图列表
         goodsInfoRecommendList:[],     //推荐商品列表
         goodsInfoHotList:[],        //热卖商品列表
+        goodsInfoGoodsList:[],        //本站所有商品列表
     },
     onLoad: function(){
         this.getGoodsInfoCarouselList();
         this.goodsInfoRecommendList();
         this.goodsInfoHotList();
+        this.goodsInfoGoodsList();
     },
 
     //获取轮播图
@@ -105,6 +107,40 @@ Page({
                 }
                 this.setData({
                     goodsInfoHotList
+                })
+            }else{
+                wx.showToast({
+                  title: res.msg,
+                  icon:'none'
+                })
+            }
+        })
+    },
+
+    //获取本站所有商品
+    goodsInfoGoodsList(){
+        request({url: '/goodsInfo/page/all?pageNum=1&pageSize=100'}).then(res => {
+            if(res.code === '0'){
+                let goodsInfoGoodsList = res.data.list;
+                if(!goodsInfoGoodsList || goodsInfoGoodsList.length === 0){
+                    goodsInfoGoodsList.push({"name":"名称1","url":this.data.defaultImageUrl});
+                    goodsInfoGoodsList.push({"name":"名称2","url":this.data.defaultImageUrl});
+                    goodsInfoGoodsList.push({"name":"名称3","url":this.data.defaultImageUrl});
+                }else{
+                    if(goodsInfoGoodsList.length>6){
+                        goodsInfoGoodsList = goodsInfoGoodsList.slice(0,6);
+                    }
+                    goodsInfoGoodsList.forEach(item => {
+                        if(!item.fields||item.fields==='[]'){
+                            item.url = this.data.defaultImageUrl;
+                        }else{
+                            let fileArr = JSON.parse(item.fields);
+                            item.url = config.baseFileUrl+fileArr[0];
+                        }
+                    });
+                }
+                this.setData({
+                    goodsInfoGoodsList
                 })
             }else{
                 wx.showToast({
